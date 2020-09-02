3036cb9a6317aed793478c61c9bec033ac732f43
Manpal-Singh <manpalsingh51@gmail.com>
2020-09-02 15:38:59 +0530

    project uploaded

---
+++ build/precache-manifest.96845c62c62d9ee1419429b46bb82a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d953568e5540af3b30d4b5bbbea770a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ce7a6a045e564bb318d",
+    "url": "/static/css/main.5c5fde5d.chunk.css"
+  },
+  {
+    "revision": "1e8326e9dd538634541d",
+    "url": "/static/js/2.a9a3c516.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.a9a3c516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce7a6a045e564bb318d",
+    "url": "/static/js/main.ae5daf62.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
